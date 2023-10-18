ba215828eeb9fe6bf2c384acb9b225a02247b8dd
Karthik <karthik.samy@anywhere.co>
2023-10-18 21:41:12 +0530

    chore: AWM-4174 updated swift tools version to 5.7

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "GoogleAnalytics",
     platforms: [
-        .iOS(.v9), .macOS(.v11), .tvOS(.v14), .watchOS(.v7)
+        .iOS(.v13), .macOS(.v10_15)
     ],
     products: [
         .library(name: "GoogleAnalytics", targets: ["GoogleAnalytics", "_GoogleAnalyticsStub"])
